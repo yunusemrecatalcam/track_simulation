5496f94a7ec8a679465df94fb34b8a0433f41b5d
yunusemrecatalcam <yunusemrecatalcam@gmail.com>
2018-03-26 13:44:17 +0300

    nice

---
+++ track.m
@@ -0,0 +1,19 @@
+%plot(xm,ym)
+
+for a= 2:1942
+    difx(a)   = xm(a) - xm((a-1));
+    dify(a)   = ym(a) - ym((a-1));    
+    %disp(difx)
+end
+drv = dify ./ difx;
+
+for i= 2:1942
+    alfa1 = atan(drv(i));
+    alfa2 = atan(drv(i-1));
+    delta_ang(i) = alfa1 -alfa2;
+    drvdif(i) = drv(i) -drv((i-1));
+end
+
+%plot(difx(1:100),dify(1:100))
+f1 =figure;
+plot(xm,delta_ang)
